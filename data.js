89471547a2007908b6eb9d399e427b44dbcf0f19
KIITF <131422174+KIITF@users.noreply.github.com>
2025-05-27 16:19:05 +0900

    Create data.js

---
+++ data.js
@@ -0,0 +1,15 @@
+const wordList = [
+  {
+    number: "310",
+    word: "砂糖",
+    reading: "さとう",
+    breakdown: "3(さ) + 10(とう)"
+  },
+  {
+    number: "104",
+    word: "イワシ",
+    reading: "いわし",
+    breakdown: "1(い) + 0(わ) + 4(し)"
+  },
+  // 必要に応じて追加
+];
